7bdca750ca26823e1e5d0af6d378f2f8ea2ca6da
DARIN NELSON <nelson.darinp@gmail.com>
2020-03-22 12:32:09 -0500

    CRUD factory

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,38 @@
 class Api::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render "index.json.jbuilder"
+  end
+
+  def create
+    @product = Product.new(
+      name: params["input_name"],
+      price: params["input_price"],
+      image_url: params["input_image_url"],
+      description: params["input_description"]
+      )
+    @product.save
+    render "show.json.jbuilder"
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render "show.json.jbuilder"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params["input_name"] || @product.name
+    @product.price = params["input_price"] || @product.price
+    @product.image_url = params["input_image_url"] || @product.image_url
+    @product.description = params["input_description"] || @product.description
+    @product.save
+    render "show.json.jbuilder"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+  end
+
 end
