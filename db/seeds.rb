2689a2fb840d752b331f74a26c1da11bef80546a
DARIN NELSON <nelson.darinp@gmail.com>
2020-03-22 11:40:34 -0500

    seeds file initialize

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Jelly", price: 3, image_url: "https://i5.walmartimages.com/asr/c314e397-5cce-4cf8-ba61-b43681e91aed_1.b787f1b08a053dda9ac46c0c5a0e6592.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF", description: "Smucker's Concortd Grape (32oz)")
+Product.create(name: "Bread", price: 2, image_url: "https://e22d0640933e3c7f8c86-34aee0c49088be50e3ac6555f6c963fb.ssl.cf2.rackcdn.com/0072250037060_CL_default_default_large.jpeg", description: "Nature's Own Honey Wheat (20oz)")
+Product.create(name: "Bananas", price: 2, image_url: "https://i.ytimg.com/vi/Km6b-RlUkfI/hqdefault.jpg", description: "Nature's Own Honey Wheat (20oz)")
