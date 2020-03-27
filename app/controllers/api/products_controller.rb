7a4e2a9d957c5419577623ad2a3581cf2f44cefd
DARIN NELSON <nelson.darinp@gmail.com>
2020-03-27 12:17:32 -0500

    Session/Order create and index

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,5 +1,5 @@
 class Api::ProductsController < ApplicationController
-  before_action :authenticate_user, except :index
+  before_action :authenticate_admin, except: [:index, :show]
 
   def index
     @products = Product.all
